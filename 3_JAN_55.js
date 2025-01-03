a8f32ac53a49160bc5e9c4fdd8b46860f8b7dfb2
unknown <ankityadav817161@gmail.com>
2025-01-03 16:22:46 +0530

    ankit

---
+++ 3_JAN_55.js
@@ -0,0 +1,14 @@
+// variables:
+//  var  is loction used to store data 
+// which is easly accessible in future use 
+//---- var
+var x=34;
+var x=40;
+var x=50;
+console.log(x);
+//-- let
+let x;// declare
+x=60;// define
+x=69;
+console.log('Output:${x}');
+const pi=3.14;
