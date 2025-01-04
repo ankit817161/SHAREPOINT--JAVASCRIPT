a480d969019f39488d123dd009983fd1e186a367
unknown <ankityadav817161@gmail.com>
2025-01-04 16:08:30 +0530

    html js

--- 3_JAN_55.js
+++ 3_JAN_55.js
@@ -2,13 +2,13 @@
 //  var  is loction used to store data 
 // which is easly accessible in future use 
 //---- var
-var x=34;
-var x=40;
-var x=50;
-console.log(x);
+//var x=34;
+//var x=40;
+//var x=50;
+//console.log(x);
 //-- let
-let x;// declare
+let x ;// declare
 x=60;// define
 x=69;
-console.log('Output:${x}');
+console.log(x);
 const pi=3.14;
---
+++ jan4.js
@@ -0,0 +1,13 @@
+let name1="IQ INDIA"
+document.write(`<center><h2>javascript</h2></center>`);
+document.write(`<center><h1>${name1}</h1></center>`);
+document.write(`<hr>`)
+
+let num1=56
+let num2=70
+let res=num1+num2
+document.write(`<h1><span style>${num1}</span>+${num2}=${res}</h1>`)
+let n1=Number(prompt("enter any number 1:"))
+let n2=Number(prompt("enter any number 2:"))
+let total=n1+n2;
+document.write(`<h1>Result=${total}</h1>`)
